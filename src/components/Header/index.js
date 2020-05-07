d7696f33e123fa1f303b9125ff878d13aeb1a3b9
Patrick <patrickperotti0@gmail.com>
2020-05-07 01:01:08 -0300

    header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo-purple.svg';
+
+import { Container, Content, Profile } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Patrick Perotti</strong>
+              <Link to="/profile">Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Perfil"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
+
+export default Header;
